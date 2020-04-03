b2f7a18354fde5faccf3bc8a3974dcf08b0566b6
allison_johnson <allison.cuttler@gmail.com>
2020-04-03 15:11:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const testVar = {}
+//const testVar = {}
 
 function testFunc() {
   return "hi"
 } //testFunc
 
-function superbowlWin(arr){
-  const winningYear = arr.find(function(elt) {
-    return elt.result === "W"
-  })
+// const record = function superbowlWin(arr){
+//   const winningYear = arr.find(function(elt) {
+//     return elt.result === "W"
+//   })
 
-  if (winningYear) {
-    return winningYear.year;
-  }
-  else {
-    return undefined;
-  }
-} //superbowlWin
+//   if (winningYear) {
+//     return winningYear.year;
+//   }
+//   else {
+//     return undefined;
+//   }
+// } //superbowlWin
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
 
