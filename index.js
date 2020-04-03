6bc6d72eb19fceaaff777aaedf35ed1a7807b38b
allison_johnson <allison.cuttler@gmail.com>
2020-04-03 14:27:52 -0700

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ const testVar = {}
 
 function testFunc() {
   return "hi"
-}
+} //testFunc
+
+function superbowlWin(arr){
+  const winningYear = arr.find(function(elt) {
+    return elt.result === "W"
+  })
+
+  if (winningYear) {
+    return winningYear.year;
+  }
+  else {
+    return undefined;
+  }
+} //superbowlWin
+
